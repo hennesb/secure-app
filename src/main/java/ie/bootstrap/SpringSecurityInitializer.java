5af296f1c71b3eea3eb3656fb2949136d272682a
hennesb <hennesb@gmail.com>
2017-04-09 22:38:42 +0100

    Adding Spring config

---
+++ src/main/java/ie/bootstrap/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package ie.bootstrap;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+  
+}
