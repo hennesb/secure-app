b06e64197922c0031e8ac037aa858706631fe26a
hennesb <hennesb@gmail.com>
2017-05-07 22:06:49 +0100

    Renaming unit test

--- src/test/java/ie/secure/eai/EAIHeadersTest.java
+++ src/test/java/ie/secure/eai/EAIHeadersTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class EAIHeadersTet {
+public class EAIHeadersTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
